961b36f20c0bbf822d54961481d80f0f461fdf55
ufukturcan-ntt <ufuk_turcan@hotmail.com>
2026-06-26 15:41:08 +0300

    Add files via upload

--- apps/frontend/public/assets/app-config.js
+++ apps/frontend/public/assets/app-config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   API_BASE_URL: "https://ntt-effort-estimator-production.up.railway.app",
-  VERSION: "2026-06-26-admin-save-config"
+  VERSION: "2026-06-26-language-sync"
 };
